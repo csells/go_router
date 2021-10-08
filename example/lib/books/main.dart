f4b9fe5796adb1fd5cc78fdf79a6b8a90d96a37f
Kevin Moore <kevmoo@google.com>
2021-10-07 19:33:36 -0700

    Drop unused imports

--- example/lib/books/main.dart
+++ example/lib/books/main.dart
@@ -3,7 +3,6 @@
 // BSD-style license that can be found in the LICENSE file.
 
 import 'package:collection/collection.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
