977da2e00e19475646b427c76e5de7316d8a7df4
Lulupointu <lucasdelsol@hotmail.fr>
2021-11-14 02:28:35 +0200

    Fix https://github.com/csells/go_router/issues/139

--- lib/src/go_route_information_parser.dart
+++ lib/src/go_route_information_parser.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 
 import 'logging.dart';
@@ -5,11 +6,15 @@ import 'logging.dart';
 /// GoRouter implementation of the RouteInformationParser base class
 class GoRouteInformationParser extends RouteInformationParser<Uri> {
   /// for use by the Router architecture as part of the RouteInformationParser
+  ///
+  ///
+  /// DO use [SynchronousFuture] so that the initial url is processed
+  /// synchronously and remove unwanted initial animations on deep-linking
   @override
-  Future<Uri> parseRouteInformation(RouteInformation routeInformation) async {
+  SynchronousFuture<Uri> parseRouteInformation(RouteInformation routeInformation) {
     log2('GoRouteInformationParser.parseRouteInformation: '
         'routeInformation.location= ${routeInformation.location}');
-    return Uri.parse(routeInformation.location!);
+    return SynchronousFuture(Uri.parse(routeInformation.location!));
   }
 
   /// for use by the Router architecture as part of the RouteInformationParser
