d1ca19835536f5338cba15ee80bf1a4c7b83a5b6
John Ryan <ryjohn@google.com>
2021-12-15 12:00:29 -0800

    Use dumpErrorToConsole for severe messages

--- go_router/lib/src/logging.dart
+++ go_router/lib/src/logging.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:developer' as developer;
+import 'package:flutter/foundation.dart';
 import 'package:logging/logging.dart';
 
 /// The logger for this package.
@@ -15,6 +16,22 @@ void setLogging({bool enabled = false}) {
   }
 
   _subscription = Logger.root.onRecord.listen((e) {
+    // Use `dumpErrorToConsole` for severe messages. This ensures that severe
+    // exceptions are formatted consistently with other Flutter examples and
+    // avoids printing duplicate exceptions.
+    if (e.level >= Level.SEVERE) {
+      final error = e.error;
+      FlutterError.dumpErrorToConsole(
+        FlutterErrorDetails(
+          exception: error is Exception ? error : Exception(error),
+          stack: e.stackTrace,
+          library: e.loggerName,
+          context: ErrorDescription(e.message),
+        ),
+      );
+      return;
+    }
+
     developer.log(
       e.message,
       time: e.time,
