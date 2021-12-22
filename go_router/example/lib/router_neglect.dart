dd75daea2a8ca08818ed517185906834525a2086
nullrocket <douglas.b.hill@gmail.com>
2021-12-22 14:12:42 -0500

    Added routerNeglect option to GoRouter. Added router_neglect.dart example. Updated examples list in docs.

---
+++ go_router/example/lib/router_neglect.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  App({Key? key}) : super(key: key);
+
+  static const title = 'GoRouter Example: Router neglect';
+
+  @override
+  Widget build(BuildContext context) =>
+      MaterialApp.router(
+        routeInformationParser: _router.routeInformationParser,
+        routerDelegate: _router.routerDelegate,
+        title: title,
+      );
+
+  final _router = GoRouter(
+    routerNeglect: true,
+    routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, state) => const Page1Screen(),
+      ),
+      GoRoute(
+        path: '/page2',
+        builder: (context, state) => const Page2Screen(),
+      ),
+    ],
+  );
+}
+
+class Page1Screen extends StatelessWidget {
+  const Page1Screen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) =>
+      Scaffold(
+        appBar: AppBar(title: const Text(App.title)),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () => context.go('/page2'),
+                child: const Text('Go to page 2'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Router.neglect(context, () {
+                  context.push('/page2');
+                  });
+                },
+                child: const Text('Go to page 2 (Navigator.push'),
+              ),
+            ],
+          ),
+        ),
+      );
+}
+
+class Page2Screen extends StatelessWidget {
+  const Page2Screen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) =>
+      Scaffold(
+        appBar: AppBar(title: const Text(App.title)),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () => context.go('/'),
+                child: const Text('Go to home page'),
+              ),
+            ],
+          ),
+        ),
+      );
+}
