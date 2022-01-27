8ab8e8ed009ce47245d4349249bc7088ef231611
Guillaume Bernos <guillaume@bernos.dev>
2022-01-27 12:15:05 +0100

    feat: create a MockGoRouter

---
+++ go_router/example/test/helpers/mock_go_router.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:go_router/src/inherited_go_router.dart';
+
+import 'package:mocktail/mocktail.dart';
+
+class MockGoRouter extends Mock implements GoRouter {}
+
+/// {@template mock_navigator_provider}
+/// The widget that provides an instance of a [MockGoRouter].
+/// {@endtemplate}
+class MockGoRouterProvider extends StatelessWidget {
+  /// {@macro mock_navigator_provider}
+  const MockGoRouterProvider({
+    required this.goRouter,
+    required this.child,
+    Key? key,
+  }) : super(key: key);
+
+  /// The mock navigator used to mock navigation calls.
+  final MockGoRouter goRouter;
+
+  /// The [Widget] to render.
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) => InheritedGoRouter(
+        goRouter: goRouter,
+        child: child,
+      );
+}
